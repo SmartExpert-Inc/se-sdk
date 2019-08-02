540b7a566e0507846a38b70636faf5e44f557380
Rostik Hvostik <g13338@gmail.com>
2019-08-02 16:58:28 +0300

    Update bots se_sdk.php

--- src/config/se_sdk.php
+++ src/config/se_sdk.php
@@ -3,5 +3,26 @@
 return [
     'posts' => [
         'host' => env('POSTS_SERVICE_HOST', null),
+    ],
+
+    'bots' => [
+        'host' => env('BOTS_SERVICE_HOST', null),
+        'telegram' => [
+            'name' => env('TELEGRAM_BOT_USERNAME', 'Telegram Bot'),
+            'link' => env('TELEGRAM_BOT_LINK', null),
+            'callback_token' => env('BOT_CALLBACK_TOKEN', null),
+            'enable' => env('TELEGRAM_ENABLE_CHAT', true),
+            'token' => env('TELEGRAM_BOT_TOKEN', null),
+        ],
+        'viber' => [
+            'name' => env('VIBER_BOT_USERNAME', 'Viber Bot'),
+            'link' => env('VIBER_BOT_LINK', null),
+            'callback_token' => env('BOT_CALLBACK_TOKEN', null)
+        ],
+        'fb' => [
+            'name' => env('FB_BOT_USERNAME', 'Facebook Bot'),
+            'link' => env('FB_BOT_LINK', null),
+            'callback_token' => env('BOT_CALLBACK_TOKEN', null)
+        ]
     ]
 ];
