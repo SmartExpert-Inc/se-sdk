44ec1374f93b682c3073abc6da778330716ae2aa
Denis Olenin <olenin.denis@gmail.com>
2019-10-11 14:21:41 +0300

    Dev Add owned skope

--- src/Traits/MultiTenant.php
+++ src/Traits/MultiTenant.php
@@ -10,4 +10,21 @@ trait MultiTenant
     {
         return session()->get('user');
     }
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeOwned($query)
+    {
+        $user = $this->getCurrentUser();
+
+        if (! $user) {
+            return $query;
+        }
+
+        return $query->where('user_id', $user->id);
+    }
 }
