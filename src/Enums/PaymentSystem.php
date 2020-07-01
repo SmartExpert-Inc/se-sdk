19133d47e4027bc25840cc5a4c9019824662f21e
Viktoria Omeltchuk <vitusik996@gmail.com>
2020-07-01 12:41:24 +0300

    CHANGE: namespace for paymentsystem enum

--- src/Enums/PaymentSystem.php
+++ src/Enums/PaymentSystem.php
@@ -1,35 +1,9 @@
 <?php
 
-namespace App\Enums;
+namespace SE\SDK\Enums;
 
 use BenSampo\Enum\Enum;
 
-/**
- * Class PaymentSystem
- *
- * @SWG\Definition(
- *     definition="PaymentSystem",
- *     description="PaymentSystem model",
- *     @SWG\Property(
- *          property="value",
- *          type="integer",
- *          description="Payment system value",
- *          example=0
- *     ),
- *     @SWG\Property(
- *          property="description",
- *          type="string",
- *          description="Payment system description",
- *          example="Фонди"
- *     ),
- *     @SWG\Property(
- *          property="key",
- *          type="string",
- *          description="Payment system key",
- *          example="Fondy"
- *     )
- * )
- */
 final class PaymentSystem extends Enum
 {
     const Fondy = 0;
